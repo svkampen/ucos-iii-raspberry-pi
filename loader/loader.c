c8d6354796f19fb1b93d24d601ecde5fd6367654
Sam van Kampen <sam@tehsvk.net>
2020-05-19 17:58:47 +0200

    [loader + sender] add checksumming

--- loader/loader.c
+++ loader/loader.c
@@ -1,4 +1,5 @@
 #include <uart.h>
+#include <startup.h>
 #include <printf.h>
 
 void int_handle_irq() {};
@@ -17,10 +18,31 @@ void main(void)
     bytes_to_load += ((uint32_t)uart_getbyte()) << 16;
     bytes_to_load += ((uint32_t)uart_getbyte()) << 24;
 
+    uint32_t bytes_to_load_copy = bytes_to_load;
+
+    uint32_t checksum = uart_getbyte();
+    checksum += ((uint32_t)uart_getbyte()) << 8;
+    checksum += ((uint32_t)uart_getbyte()) << 16;
+    checksum += ((uint32_t)uart_getbyte()) << 24;
+
+
     uint8_t* offset = (uint8_t*)0x8000;
+
     while (bytes_to_load--)
     {
         *offset++ = uart_getbyte();
+    }
+
+    uint32_t recomputed_checksum = 0;
+    while (bytes_to_load_copy--)
+    {
+        recomputed_checksum += *(--offset);
+    }
+
+    if (recomputed_checksum != checksum)
+    {
+        printf("Checksum failed: got %lu, expected %lu\n", recomputed_checksum, checksum);
+        hang();
     }
 
     while (uart_getbyte() != 'l');
--- send.py
+++ send.py
@@ -12,7 +12,14 @@ data = f.read()
 size = len(data)
 
 length = struct.pack('<I', size)
+
+checksum = 0
+for b in data:
+    checksum += b
+    checksum %= 2**32
+
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
     stdout.write(length)
+    stdout.write(struct.pack('<I', checksum))
     stdout.write(data)
     stdout.flush()
