1fe0f137c0aca187ea126c4c8d7176be9c52a66b
Sam van Kampen <sam@tehsvk.net>
2020-06-01 16:17:40 +0200

    wait_for_cycles: division by two so we actually approximately wait for the input argument in cycles

--- source/wait.s
+++ source/wait.s
@@ -4,12 +4,14 @@
 .extern uart_init
 .extern deltas
 
-// Waits for 2*r0 + 4/5 (return) + 6 (mispredict) + 0/3 (static branch)
-// (2*r0 + 10 or +11 or +13 or +14)
+// Waits for r0 + 4/5 (return) + 6 (mispredict) + 0/3 (static branch)
+// (r0 + 10 or +11 or +13 or +14)
 // Preconditions: r0 > 0
 wait_for_cycles:
+    lsr r0,r0,#1        // r0 /= 2
+wfc_loop$:
     subs r0, r0, #1     // 1 cycle
-    bne wait_for_cycles // 1 cycle (4 eerste keer program-wide (static), 7 laatste keer (mispredict))
+    bne wfc_loop$       // 1 cycle (4 eerste keer program-wide (static), 7 laatste keer (mispredict))
 end$:
     bx lr               // 4/5 cycles, we don't edit lr but the return stack may be empty because it was used during an interrupt
 
