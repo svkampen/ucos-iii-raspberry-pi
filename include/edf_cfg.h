7ef1e9483936b86dd8e4a7dda814664353949fb0
Sam van Kampen <sam@tehsvk.net>
2020-06-01 16:24:16 +0200

    Deadline miss checks aan

--- include/edf_cfg.h
+++ include/edf_cfg.h
@@ -2,14 +2,12 @@
 
 /* Power-of-two minus one, please. */
 #define EDF_CFG_MAX_TASKS 255
-#define EDF_CFG_CHECK_DEADLINE_MISS 0u
+#define EDF_CFG_CHECK_DEADLINE_MISS 1u
 #define EDF_CFG_ENABLED 1u
 #define EDF_CFG_DEBUG 0u
 
 #define EDF_DEADLINE_ABSOLUTE(tcb)                                             \
     ((tcb)->EDFCurrentActivationTime + (tcb)->EDFRelativeDeadline)
-
-#define TICKS_TO_USEC(ticks) ((1000000 * ticks) / OSCfg_TickRate_Hz)
 
 #define OS_EDF_HEAP_FOREACH(body)                                              \
     for (int32_t idx = 0; idx < OSEdfHeapSize; ++idx)                         \
